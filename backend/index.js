6e92f6631332b0da3a3f18ea49760d31c8571436
naitikagarwal-12 <naitikagarwal.938@gmail.com>
2026-02-08 03:40:00 +0530

    Added the backend routes, login, register, contact us and integrated in frontend

--- backend/index.js
+++ backend/index.js
@@ -1,44 +1,32 @@
-import express from "express";
-import cors from "cors";
-import dotenv from "dotenv";
-import connectDB from "./config/db.js";
-import newsRoutes from "./routes/newsRoutes.js";
-import { syncNews } from "./services/newsSync.js";
-import { startNewsCron } from "./crone/syncjob.js";
+import dotenv from 'dotenv';
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import authRoutes from './routes/auth.js';
+import contactRoutes from './routes/contact.js';
 
 dotenv.config();
 
 const app = express();
 
-
-app.use(cors());
 app.use(express.json());
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true
+}));
 
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log('Connected to Database'))
+    .catch((err) => console.error('Database connection error:', err));
 
-app.use("/api/news", newsRoutes);
+app.use('/api/auth', authRoutes);
+app.use('/api/contact', contactRoutes);
 
-
-app.get("/", (req, res) => {
-  res.send("API is running...");
+app.get('/', (req, res) => {
+    res.send('seekAyurveda Backend is Running');
 });
 
-
-const startServer = async () => {
-  try {
-    
-    await connectDB();
-    
-    
-    startNewsCron();
-    
-    const PORT = process.env.PORT || 5000;
-    app.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`);
-    });
-  } catch (error) {
-    console.error("Failed to start server:", error.message);
-    process.exit(1);
-  }
-};
-
-startServer();
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
